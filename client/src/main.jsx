4255a2158960496318531627887c8c29adbd0f75
Rakhi <rakhikamboz7@gmail.com>
2025-06-14 17:35:00 +0530

    Fix SPA route handling for admin-dashboard

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import "./i18n/i18n"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
